6d8fc0313b8a3a1b067669c1313f0cd82f36ea33
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-05 13:51:34 -0500

    Intial setup

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,1 +1,15 @@
 // TODO: Spin up a node server - https://nodejs.org/en/docs/guides/getting-started-guide/
+const http = require("http");
+
+const hostName = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World");
+});
+
+server.listen(port, hostName, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
