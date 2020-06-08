e923a6453a9705248bdeda07c728bda9f48b33f0
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-07 20:21:47 -0500

    Set up name to be displayed, report back for bug fixes

--- src/components/index.js
+++ src/components/index.js
@@ -11,7 +11,23 @@ import { PhoneNumber } from "./user_info";
 import { grabRandomUsers } from "api";
 
 export class components extends React.Component {
-  state = {};
+  state = {
+    Address: false,
+    Birthday: false,
+    Email: false,
+    Name: true,
+    Password: false,
+    PhoneNumber: false,
+    results: [],
+  };
+
+  async componentDidMount() {
+    try {
+      this.setState({ products: await grabRandomUsers() });
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   render() {
     return (
--- src/components/user_info/Name.js
+++ src/components/user_info/Name.js
@@ -2,12 +2,15 @@ import PropTypes from "prop-types";
 import React from "react";
 
 export const Name = ({ results }) => {
-  return (
-    <div>
-      <button type="button">Name</button>
-      <p></p>
-    </div>
-  );
+  return results.map(({ name }, index) => {
+    return (
+      <div key={index}>
+        <button type="button">Name</button>
+        <p>Hello, My name is</p>
+        <p>{name}</p>
+      </div>
+    );
+  });
 };
 
 Name.propTypes = {
