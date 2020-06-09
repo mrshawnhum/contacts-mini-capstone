5940e91ae08b724caedd301ec44266670bf24151
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-09 13:03:19 -0500

    Remove files

--- src/components/user_info/index.js
+++
@@ -1,6 +0,0 @@
-export { Address } from "./Address";
-export { Birthday } from "./Birthday";
-export { Email } from "./Email";
-export { Name } from "./Name";
-export { Password } from "./Password";
-export { PhoneNumber } from "./PhoneNumber";
