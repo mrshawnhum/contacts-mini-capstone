2f4922df8b1d53ec77a4f2f69443a36f6b1369ad
Shawn Humphreys <shawnhum97@gmail.com>
2020-06-09 18:06:26 -0500

    Add toggle css
    Toggle css
    
    
    Change font size

--- src/components/ContactCard/Toggle.js
+++ src/components/ContactCard/Toggle.js
@@ -3,7 +3,26 @@ import React from "react";
 
 import "font-awesome/css/font-awesome.min.css";
 
-export const Toggle = ({ icon }) => <span className={`fa fa-${icon}`} />;
-Toggle.propTypes = {
-  icon: PropTypes.string.isRequired,
-};
+import "./Toggle.css";
+import styles from "./Toggle.module.css";
+
+export class Toggle extends React.Component {
+  static propTypes = {
+    icon: PropTypes.string.isRequired,
+    info: PropTypes.string,
+    stProxy: PropTypes.object,
+  };
+
+  handleClick = () => {
+    this.props.stProxy.activeInfo = this.props.info;
+  };
+
+  render() {
+    const className =
+      this.props.stProxy.activeInfo === this.props.info
+        ? `Toggle fa fa-${this.props.icon} ${styles.active}`
+        : `Toggle fa fa-${this.props.icon}`;
+
+    return <span className={className} onClick={this.handleClick} />;
+  }
+}
